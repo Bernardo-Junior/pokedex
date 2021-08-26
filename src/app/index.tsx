d134032c38b84f3b305d826a339cbdbe417bbf88
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-08-25 23:22:31 -0400

    fix: resolvendo problema do reanimated

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import 'react-native-gesture-handler';
+
+// import { Container } from './styles';
+
+const app: React.FC = () => {
+  return <View />;
+}
+
+export default app;
