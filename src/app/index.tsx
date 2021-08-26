95bc57b3eb52b225aab8b2855a4bbf188d3f5cdd
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-08-26 18:19:51 -0400

    chore: adicionando statusBar do android e cor principal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { useEffect } from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Platform, View } from 'react-native';
 import 'react-native-gesture-handler';
 
 import SplashScreen from 'react-native-splash-screen';
+import { ColorPrimary } from '../utils/global';
 
 // import { Container } from './styles';
 
@@ -16,6 +18,11 @@ const app: React.FC = () => {
 
   return (
     <NavigationContainer>
+      {
+        Platform.OS === "android" && (
+          <StatusBar backgroundColor={ColorPrimary} barStyle="light-content"/>
+        )
+      }
       <View />
     </NavigationContainer>
   )
---
+++ src/utils/global.ts
@@ -0,0 +1,1 @@
+export const ColorPrimary = "#ED5564";
